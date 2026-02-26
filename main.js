d7374cf8f7f09c925267d962029bb18ee7beea38
sebastian-nunez-latbc <sebastian-nunez@latbc.com.mx>
2026-02-26 22:17:38 +0000

    add html css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+function getRandomHexColor() {
+  const randomNumber = Math.floor(Math.random() * 0xffffff);
+  return "#" + randomNumber.toString(16).padStart(6, "0");
+}
+
+function changeColor() {
+    const btn = document.getElementById('btn')
+    const randomHex = getRandomHexColor()
+
+    btn.style.color = randomHex
+}
